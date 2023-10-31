05de8ec65809576a52971c6eeefca6bb09fd08a8
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2023-11-01 00:05:44 +0100

    put timer class in file

--- timer.cpp
+++ timer.cpp
@@ -1,1 +1,51 @@
 // put timer class here
+
+#include<thread>
+
+
+class Timer
+{
+private:
+    bool active;
+public:
+    Timer() {
+        this->active = true;
+    }
+    ~Timer() {
+
+    }
+    void setTimeout(auto func, int delay) {
+        this->active = true;
+        std::thread t([=, *this] () {
+            if(this->active == false) return;
+            std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+            if(this->active == false) return;
+            func();
+        });
+        t.detach();
+    }
+    void setInterval(auto func, int interval) {
+        this->active = true;
+        std::thread t([=, *this] () {
+            while(true) {
+                if(this->active == false) return;
+                std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+                if(this->active == false) return;
+                func();
+            }
+        });
+        t.detach();
+    }
+
+    void stop() {
+        this->active = false;
+    }
+
+    void stop(int delay) {
+        this->setTimeout([&]() {this->stop();}, delay);
+    }
+
+    bool isActive() {
+        return this->active;
+    }
+};
